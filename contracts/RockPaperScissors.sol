09b4e25885055f01443131f7fd5330b69d0e2e70
RCantu92 <Roberto_Cantu@live.com>
2021-05-23 16:15:06 -0500

    Add functionality that handles games that end in a draw

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-// PERHAPS NOT NEEDED?
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract RockPaperScissors {
@@ -28,6 +27,11 @@ contract RockPaperScissors {
         address payable playerAddress;
         string chosenItem;
     }
+
+    // Mapping that stores whether player's last
+    // game resulted in a tie
+    // RETURN TO INTERNAL
+    mapping(address => bool) /*internal*/ public playerVoucher;
 
     /*
     // FUNCTION ONLY FOR TESTING
@@ -57,8 +61,11 @@ contract RockPaperScissors {
             "You have either not input an appropriate item or item name not written in lowercase."
         );
 
-        // Transfer a portion of one $LINK from player to contract address
-        kovanLink.transferFrom(msg.sender, address(this), 50000000);
+        // If players most recent game did not result in a tie,
+        // transfer a portion of one $LINK from player to contract address
+        if (playerVoucher[msg.sender] != true) {
+            kovanLink.transferFrom(msg.sender, address(this), 50000000);
+        }
 
         // Check to see if a player has already
         // submitted item, if not, that player is
@@ -70,7 +77,9 @@ contract RockPaperScissors {
             // Call function that has paper, rock, scissors, logic
             gameLogic();
             
-            playerNumber ++;
+            // SINCE THE PLAYERNUMBER VARIABLE IS SET
+            // TO 0, THIS LINE MAY BE UNECCESSARY
+            // playerNumber ++;
         } else {
             players[playerNumber] = Player(payable(msg.sender), _chosenItem);
             playerNumber ++;
@@ -84,37 +93,49 @@ contract RockPaperScissors {
         if(areItemsEqual(players[0].chosenItem, "rock") == true) {
             // If then player two chose "paper"
             if(areItemsEqual(players[1].chosenItem, "paper") == true) {
+                removeVoucher(players[0].playerAddress, players[1].playerAddress);
                 disperseWinnings(players[1].playerAddress);
             // If then player two chose "scissors"
             } else if(areItemsEqual(players[1].chosenItem, "scissors") == true) {
+                removeVoucher(players[0].playerAddress, players[1].playerAddress);
                 disperseWinnings(players[0].playerAddress);
             // If then player also chose "rock"
             } else {
-                // Tie, both players chose "rock"
+                // Give players a voucher to play again
+                // without sending in $LINK
+                givePlayersVoucher(players[0].playerAddress, players[1].playerAddress);
             }
         // Outcomes for if first player chose "paper"
         } else if(areItemsEqual(players[0].chosenItem, "paper") == true) {
             // If then player two chose scissors
             if(areItemsEqual(players[1].chosenItem, "scissors") == true) {
+                removeVoucher(players[0].playerAddress, players[1].playerAddress);
                 disperseWinnings(players[1].playerAddress);
             // If then player two chose "rock"
             } else if(areItemsEqual(players[1].chosenItem, "rock") == true) {
+                removeVoucher(players[0].playerAddress, players[1].playerAddress);
                 disperseWinnings(players[0].playerAddress);
             // If then player also chose "paper"
             } else {
-                // Tie, both players chose "paper"
+                // Give players a voucher to play again
+                // without sending in $LINK
+                givePlayersVoucher(players[0].playerAddress, players[1].playerAddress);
             }
         // Outcome for if first player chose "scissors"
         } else {
             // If then player two chose "rock"
             if(areItemsEqual(players[1].chosenItem, "rock") == true) {
+                removeVoucher(players[0].playerAddress, players[1].playerAddress);
                 disperseWinnings(players[1].playerAddress);
             // If then player two chose "paper"
             } else if(areItemsEqual(players[0].chosenItem, "paper") == true) {
+                removeVoucher(players[0].playerAddress, players[1].playerAddress);
                 disperseWinnings(players[0].playerAddress);
             // If then player also chose "scissors"
             } else {
-                // Tie, both players chose "scissors"
+                // Give players a voucher to play again
+                // without sending in $LINK
+                givePlayersVoucher(players[0].playerAddress, players[1].playerAddress);
             }
         }
     }
@@ -124,11 +145,34 @@ contract RockPaperScissors {
     function disperseWinnings(address _winner) internal {
         // Reset number of players
         playerNumber = 0;
+        
         // Store most recent winner
         _mostRecentWinner = _winner;
 
         // Player two wins the $LINK
         kovanLink.transferFrom(address(this), _winner, 100000000);
+    }
+
+    // Function that gives a player a voucher
+    // to play a game without sending $LINK
+    // to contract
+    function givePlayersVoucher(address _tyingPlayerOne, address _tyingPlayerTwo) internal {
+        // Reset number of players
+        playerNumber = 0;
+        
+        // Store player address
+        // so that next game, they
+        // don't have to transfer $LINK
+        // to the contract
+        playerVoucher[_tyingPlayerOne] = true;
+        playerVoucher[_tyingPlayerTwo] = true;
+    }
+    
+    // Function to confirm players have no voucher
+    // after completing legitimate game
+    function removeVoucher(address _playerOneAddress, address _playerTwoAddress) internal {
+        playerVoucher[_playerOneAddress] = false;
+        playerVoucher[_playerTwoAddress] = false;
     }
 
     // Function that compares the
